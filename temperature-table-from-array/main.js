e359aa10ab6f77135788ec040a917efde64554da
davorpa <davorpatech@gmail.com>
2020-11-16 22:09:04 +0100

    🚧 MOD 04 - Temperatures table from Array
    - Agregar estructura del proyecto
    - Agregar enunciado del ejercicio

---
+++ temperature-table-from-array/main.js
@@ -0,0 +1,73 @@
+'use strict';
+
++(function() {
+
+// Temp. menor que 20: fondo azul
+// Temp. entre 20 y 30: fondo naranja
+// Temp. mayor de 30: fondo rojo
+
+const temperaturas = [
+    {
+        city: "A coruña",
+        min: 17,
+        max: 23,
+    },
+    {
+        city: "Ferrol",
+        min: 15,
+        max: 32,
+    },
+    {
+        city: "Lugo",
+        min: -20,
+        max: 31,
+    },
+    {
+        city: "Ourense",
+        min: 18,
+        max: 35,
+    },
+    {
+        city: "Pontevedra",
+        min: 18,
+        max: 29,
+    },
+    {
+        city: "Antártida",
+        min: -50,
+        max: -10,
+    },
+];
+
+/*
+<table>
+    <caption>Temperaturas</caption>
+    <thead>
+        <th>Ciudad</th>
+        <th>Mínima</th>
+        <th>Máxima</th>
+    </thead>
+    <tr>
+        <td>A Coruña</td>
+        <td>17</td>
+        <td>23</td>
+    </tr>
+    tr....
+</table>
+*/
+
+/*
+ * *************************
+ *     UTIL FUNCTIONS
+ * *************************
+ */
+
+
+/*
+ * *************************
+ *        MAIN RUN
+ * *************************
+ */
+
+
+}());
